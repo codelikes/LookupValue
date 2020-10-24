d3898ddb35793a9abc5119a82ea6a56c5760f666
Nikolay Ponich <n.ponich@gmail.com>
2020-10-24 14:39:30 +0300

    init commit

---
+++ tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Codelikes\LookupValue\Test;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+class TestCase extends PHPUnitTestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true, 'first true test');
+    }
+}
